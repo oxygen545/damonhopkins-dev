4b9edd1834e692d3edbd4cf3824c23e779bf0baf
oxygen545 <damon@hopkins-family.org>
2018-11-08 08:26:07 -0600

    form styling

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,2 +1,6 @@
 class Message < ApplicationRecord
+	validates :username, presence: true
+	validates :email, presence: true
+	validates :subject, presence: true
+	validates :body, presence: true
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,4 +1,6 @@
 class Photo < ApplicationRecord
-has_one_attached :image
-
+	has_one_attached :image
+	validates :page_name, presence: true
+	validates :image, presence: true
+	validates :sequence, numericality: { only_integer: true }
 end
